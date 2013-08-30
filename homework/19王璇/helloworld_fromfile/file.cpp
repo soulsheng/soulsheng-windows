108b88d45db5001ac00669a5e1880dd554c0c5bd
soulsheng <soulsheng@qq.com>
2013-08-30 17:06:57 +0800

    提交作业 王璇

---
+++ homework/19王璇/helloworld_fromfile/file.cpp
@@ -0,0 +1,16 @@
+#include "windows.h"
+#include "stdio.h"
+#include "windows.h"
+char line[30];
+int WINAPI WinMain (_In_  HINSTANCE hInstance,
+  _In_  HINSTANCE hPrevInstance,
+  _In_  LPSTR lpCmdLine,
+  _In_  int nCmdShow)
+   { FILE *fp;
+    fp=fopen("file.txt", "r");
+	if(fp!=NULL){
+    fread(line,sizeof(char),12,fp);
+	MessageBox(0,line,"windows´°¿Ú",0);
+	}
+		return 0;
+}
---
+++ homework/19王璇/winhello/winhello.cpp
@@ -0,0 +1,9 @@
+#include "windows.h"
+int WINAPI WinMain (_In_  HINSTANCE hInstance,
+  _In_  HINSTANCE hPrevInstance,
+  _In_  LPSTR lpCmdLine,
+  _In_  int nCmdShow)
+{
+	MessageBox(0,"hello world!","windows´°¿Ú",0);
+		return 0;
+}

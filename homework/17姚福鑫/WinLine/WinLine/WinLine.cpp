65903f8691a1811a4a236acb3edc58c93cb7787e
soulsheng <soulsheng@qq.com>
2013-08-31 13:59:03 +0800

    补充作业 姚福鑫 画折线

--- homework/17姚福鑫/WinLine/WinLine/WinLine.cpp
+++ homework/17姚福鑫/WinLine/WinLine/WinLine.cpp
@@ -1,8 +1,9 @@
-// 3.cpp : 定义应用程序的入口点。
+// WinPaint.cpp : 定义应用程序的入口点。
 //
 
 #include "stdafx.h"
-#include "3.h"
+#include "WinLine.h"
+#include <Windowsx.h>
 
 #define MAX_LOADSTRING 100
 
@@ -10,40 +11,41 @@
 HINSTANCE hInst;								// 当前实例
 TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
 TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
-char text[1024];
-int nCount;
+int xPoint[1024],yPoint[1024];                  //用来保存点的坐标
+int count;                                      //计数器
+RECT rect;
+
 // 此代码模块中包含的函数的前向声明:
-ATOM				CustomWindowDIY(HINSTANCE hInstance);
+ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
- 	// TODO: 在此放置代码。
+	// TODO: 在此放置代码。
 	MSG msg;
 	HACCEL hAccelTable;
 
-	// 初始化全局字符串
+
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_MY3, szWindowClass, MAX_LOADSTRING);
-	CustomWindowDIY(hInstance);
-	/*MyRegisterClass(hInstance);*/
+	LoadString(hInstance, IDC_WINLINE, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
 	if (!InitInstance (hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
 
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY3));
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINLINE));
 
-	// 主消息循环:
+
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -58,20 +60,9 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
 
 
+
 //
-//  函数: MyRegisterClass()
-//
-//  目的: 注册窗口类。
-//
-//  注释:
-//
-//    仅当希望
-//    此代码与添加到 Windows 95 中的“RegisterClassEx”
-//    函数之前的 Win32 系统兼容时，才需要此函数及其用法。调用此函数十分重要，
-//    这样应用程序就可以获得关联的
-//    “格式正确的”小图标。
-//
-ATOM CustomWindowDIY(HINSTANCE hInstance)
+ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
 
@@ -82,71 +73,51 @@ ATOM CustomWindowDIY(HINSTANCE hInstance)
 	wcex.cbClsExtra		= 0;
 	wcex.cbWndExtra		= 0;
 	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY3));
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINLINE));
 	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
 	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_MY3);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_WINLINE);
 	wcex.lpszClassName	= szWindowClass;
 	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
 	return RegisterClassEx(&wcex);
 }
 
-//
-//   函数: InitInstance(HINSTANCE, int)
-//
-//   目的: 保存实例句柄并创建主窗口
-//
-//   注释:
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
+
+
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{ 
- for (int i=0; i<1024; i++)
-	{
-		text[i] = 0;
+{
+	for(int i=0;i<1024;i++){
+		xPoint[i]=0;
+		yPoint[i]=0;
 	}
-	nCount = 0;
+	count = 0;
 
-	strcpy(text, "Hello World");
-	nCount = strlen("Hello World");
 
-   HWND hWnd;
+	HWND hWnd;
 
-   hInst = hInstance; // 将实例句柄存储在全局变量中
+	hInst = hInstance; // 将实例句柄存储在全局变量中
 
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
 
-//
-//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的: 处理主窗口的消息。
-//
-//  WM_COMMAND	- 处理应用程序菜单
-//  WM_PAINT	- 绘制主窗口
-//  WM_DESTROY	- 发送退出消息并返回
-//
-//
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
 	HDC hdc;
-	RECT rect,rectDIY;
 
 	switch (message)
 	{
@@ -166,29 +137,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 		break;
+
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
-		rectDIY.left = 0;
-		rectDIY.right = 500;
-		rectDIY.top = 0;
-		rectDIY.bottom = 500;
-	/*TextOut(hdc,0,0,text,nCount);*/
-	DrawText(hdc,text,nCount,&rectDIY,0);
 		// TODO: 在此添加任意绘图代码...
+		for(int i=1;i<count;i++)
+		{
+			MoveToEx(hdc,xPoint[i-1],yPoint[i-1],0);
+			LineTo(hdc,xPoint[i],yPoint[i]);
+		}
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
-		MessageBox(0,"Save","提示",0);
 		PostQuitMessage(0);
 		break;
-    case WM_CHAR:
-		text[nCount]= wParam;
-		nCount ++;
-		
-		GetClientRect(hWnd, &rect);
-		InvalidateRect(hWnd, &rect, TRUE);
-
+	case WM_MOUSEMOVE:
+		xPoint[count] = GET_X_LPARAM(lParam);
+		yPoint[count] = GET_Y_LPARAM(lParam);
+		count++;
+		GetClientRect(hWnd,&rect);
+		InvalidateRect(hWnd,&rect,true);
 		break;
+
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
@@ -214,3 +184,4 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	}
 	return (INT_PTR)FALSE;
 }
+

2c8f0a198b2848032139673d37c85561d46e982f
soulsheng <soulsheng@qq.com>
2013-08-31 15:14:14 +0800

    新增demo 上课演示

---
+++ demo-WindowsHello/WinHello/WinHello.cpp
@@ -0,0 +1,10 @@
+#include "windows.h"
+
+int WINAPI WinMain( _In_  HINSTANCE hInstance,
+	_In_  HINSTANCE hPrevInstance,
+	_In_  LPSTR lpCmdLine,
+	_In_  int nCmdShow)
+{
+    MessageBox(0, "Hello World", "Windows´°¿Ú", 0);
+	return 0;
+}
